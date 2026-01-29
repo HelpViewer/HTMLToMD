38149d9aabd5a4a86c03400bc3a865e72bed1c96
VSimon <vita.simon@centrum.cz>
2026-01-29 11:54:51 +0100

    HTMLToMD : li fix of missing newline

--- HTMLToMD.js
+++ HTMLToMD.js
@@ -26,7 +26,7 @@ function HTMLToMD(parent, ctx) {
       
       const itemMarker = node.parentElement.tagName.toLowerCase() != 'ol' ? '-' : `${ctx.listCounter[ctx.listStack.length-1]}.`;
       //${'  '.repeat(Math.max(0, ctx.listStack.length-1))}
-      const reply = `${itemMarker} ${children}`;
+      const reply = `${itemMarker} ${children}\n`;
 
       if (!node.nextElementSibling) {
         ctx.listStack.pop();
